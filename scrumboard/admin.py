3082c538c26f08ae05a277d15faf2511eddd4e95
Harshith bhargava reddy potu <harshith.potu@gmail.com>
2017-10-11 21:45:44 -0500

    Trying Django REST

---
+++ scrumboard/admin.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from models import *
+
+
+# Register your models here.
+
+class ListAdmin(admin.ModelAdmin):
+
+    pass
+
+class CardAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Card, CardAdmin)
